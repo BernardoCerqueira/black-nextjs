6b52ce8f14a236bbea1527c19faf87007f37fade
Bernardo Cerqueira <katanardo@gmail.com>
2024-11-29 19:14:31 -0300

    Criando API com Next.js

---
+++ src/pages/api/products/[id].ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import products from "../../../../database.json"
+
+export default function handler(req: NextApiRequest, res: NextApiResponse){
+    const {id} = req.query 
+    const product = products.find(prod => prod.id === Number(id))
+    if(!product) res.status(404).json({message: "Product not found."})
+    res.status(200).json(product)
+
+    
+}
