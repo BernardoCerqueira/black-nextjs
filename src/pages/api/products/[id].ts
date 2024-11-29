27b50494aa26a7f07f2ce4c8a750e91d74b96587
Bernardo Cerqueira <katanardo@gmail.com>
2024-11-29 19:24:54 -0300

    Update [id].ts

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -4,7 +4,7 @@ import products from "../../../../database.json"
 export default function handler(req: NextApiRequest, res: NextApiResponse){
     const {id} = req.query 
     const product = products.find(prod => prod.id === Number(id))
-    if(!product) res.status(404).json({message: "product not found."})
+    if(!product) res.status(404).json({message: "Product not found."})
     res.status(200).json(product)
 
     
